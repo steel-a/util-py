5ce021dc54792ea86bed50019a6ef1f8e8ba98b1
steel-a <stell4b@gmail.com>
2020-06-02 20:43:51 -0300

    fix getMatches function

--- regex.py
+++ regex.py
@@ -1,7 +1,16 @@
 import re
 
 def getMatches(pattern:str, text:str):
-    return re.findall(pattern, text)
+    lst = re.findall(pattern, text)
+    if lst != []:
+        if isinstance(lst[0],tuple):
+            return lst
+        else:
+            lst2 = []
+            for e in lst:
+                lst2.append([e])
+            return lst2
+    return lst
 
 def getValue(pattern:str, text:str, i:int=0):
     match = re.match(pattern, text)
