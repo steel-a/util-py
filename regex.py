79c9b9292dc010e75efe5b329e5535af9c8087d1
steel-a <stell4b@gmail.com>
2020-06-03 19:48:58 -0300

    Regex class created

--- regex.py
+++ regex.py
@@ -1,22 +1,27 @@
 import re
 
-def getMatches(pattern:str, text:str):
-    lst = re.findall(pattern, text)
-    if lst != []:
-        if isinstance(lst[0],tuple):
-            return lst
-        else:
-            lst2 = []
-            for e in lst:
-                lst2.append((e,))
-            return lst2
-    return lst
+class Regex:
 
-def getValue(pattern:str, text:str, i:int=0):
-    match = re.match(pattern, text)
-    if match is not None:
-        try:
-            return match[i]
-        except:
-            return None
-    return None
+    def __init__(self, pattern:str):
+        self.pattern = pattern
+
+    def getMatches(self, text:str):
+        lst = re.findall('('+self.pattern+')', text)
+        if lst != []:
+            if isinstance(lst[0],tuple):
+                return lst
+            else:
+                lst2 = []
+                for e in lst:
+                    lst2.append((e,))
+                return lst2
+        return lst
+
+    def getValue(self, text:str, i:int=0):
+        match = re.match(self.pattern, text)
+        if match is not None:
+            try:
+                return match[i]
+            except:
+                return None
+        return None
