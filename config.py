596f39b4846f036f6b906ed754cd5028e84615fd
steel-a <stell4b@gmail.com>
2020-06-07 14:54:29 -0300

    change dict from config to item

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ from utilpy.regex import Regex
 class Config:
 
     def __init__(self, name:str):
-        self.config = dict()
+        self.item = dict()
 
         r = Regex(r'[ \t]*([a-zA-Z0-9-]*)[ \t]*=[ \t]*([a-zA-Z0-9-+*\/=?!@#$%&()_{}<>:;,.~^`"\' ]*)[ \t]*')
 
         for m in r.getMatches(File(name).read()):
-            self.config[m[1]] = m[2]
+            self.item[m[1]] = m[2]
