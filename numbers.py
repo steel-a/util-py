61746b9c8c8a3c8cef0f3e536547ddfc1e6cf5ce
steel-a <stell4b@gmail.com>
2020-07-06 20:30:54 -0300

    toFloat calculates percentage

--- numbers.py
+++ numbers.py
@@ -8,24 +8,25 @@ def toFloat(value:str) -> float:
     ,'1000000000,76','1000000000,7','1,000,000,000','1.000.000.000','1000000000', '10', '1,1', '1.0', '1,11', '1.01'
     ,1,000,000,000.765','1000000000.765','1.000.000.000,765', '1000000000,765,'1,000.765','1000.765','1.000,765', '1000,765'
     """
-    value = value.replace('$','').replace('USD','').replace('US','').replace('BRL','').replace('BR','').replace('EUR','').replace('R','').replace(' ','')
+    div = 1 if value.count('%')==0 else 100
+    value = value.replace('$','').replace('USD','').replace('US','').replace('BRL','').replace('BR','').replace('EUR','').replace('R','').replace(' ','').replace('%','')
     num_coma = value.count(',')
     num_dot = value.count('.')
 
     try:
 
         if num_coma>1: # US Format
-            return float(value.replace(',',''))
+            return float(value.replace(',',''))/div
         elif num_dot>1: # European Format
-            return float(value.replace('.','').replace(',','.'))
+            return float(value.replace('.','').replace(',','.'))/div
         elif num_coma==1 and num_dot==1:
             if(value.find(',')<value.find('.')): # US Format
-                return float(value.replace(',',''))
+                return float(value.replace(',',''))/div
             else: # European format
-                return float(value.replace('.','').replace(',','.'))
+                return float(value.replace('.','').replace(',','.'))/div
         elif num_coma==1: # European Format
-            return float(value.replace(',','.'))
+            return float(value.replace(',','.'))/div
         elif num_dot==1 or (num_coma==0 and num_dot==0): # US Format
-            return float(value)
+            return float(value)/div
     except:
         return None
